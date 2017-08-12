791a8233eaf71a997a4033ee9bfcad113436fa41
Carol Chen <carolyfisher@gmail.com>
2017-08-12 14:24:38 -0400

    Finished answer check

--- modules/answercheck/answercheck.js
+++ modules/answercheck/answercheck.js
@@ -9,32 +9,36 @@ exports.run = function(api, event) {
           }
 
         var response = function(res) {
-            var keywords = JSON.stringify(res);
-            api.sendMessage(keywords, event.thread_id);
+            var keywords = Object.keys(res)
+            // api.sendMessage(keywords, event.thread_id);
             indico.keywords(event.body.substring(2))
             .then(function(res) {
                 var answerKeywords = Object.keys(res);
-                // api.sendMessage(keywords, event.thread_id);
                 var boop = 0 ;
                 for (var i = 0; i < keywords.length; i++){
-                    if (answerKeywords.indexOf(keywords[i]) > -1){
+                    if (answerKeywords.indexOf(keywords[i].toLowerCase()) > -1){
                         boop++;
                     }
                 }
+                // api.sendMessage(answerKeywords, event.thread_id);
                 if (boop / keywords.length > .75){
                     api.sendMessage("You got it right!", event.thread_id)
                 }
                 else{
-                    api.sendMessage("IT'S WRONG", event.thread_id)
+                    api.sendMessage("IT'S WRONG. THE CORRECT ANSWER WAS " + data, event.thread_id)
                 }
 
             }).catch(logError);
         }
         var logError = function(err) { api.sendMessage(err, event.thread_id); }
+        if (data.toLowerCase().replace(/ /g,'') == event.body.substring(2).toLowerCase().replace(/ /g,'')){
+            api.sendMessage("YOU GO IT RIGHT OMG YOU'RE SO SMART WOW", event.thread_id);
+        }
+        else{// single example
+            indico.keywords(data)
+              .then(response)
+              .catch(logError);
+        }
+    });
 
-        // single example
-        indico.keywords(data)
-          .then(response)
-          .catch(logError);
-        });
 };
