9ae6d75c07cde831f5200c8a05a25555785ac493
“faizaanmadhani” <fzmadhani@gmail.com>
2017-08-12 12:17:57 -0400

    Ask.js integration lmao

--- modules/ask/ask.js
+++ modules/ask/ask.js
@@ -1,10 +1,13 @@
 var fs = require('fs');
-
-function obj (name, question, answer) {
-  this.name = name;
-  this.question = question;
-  this.answer = answer;
-}
+// var request = require('request');
+// var i = 0;
+// var indexes = {};
+//
+var Nogal = function (n, q, a) {
+    this.n = n
+    this.q = q
+    this.a = a;
+};
 
 exports.run = function(api, event) {
   api.sendMessage("Wonderfulness! This works?", event.thread_id);
@@ -16,20 +19,40 @@ exports.run = function(api, event) {
     var question = word[0];
     var answer = word[1];
 
-    var brian = new obj(event.sender_id, question, answer);
-    api.sendMessage(JSON.stringify((brian), event.thread_id);
-    var jsonData = JSON.stringify(brian)
+    var kailas = new Nogal(event.sender_id, question, answer);
 
-    // fs.writeFile("storage.json", jsonData, function(err) {
-    // if(err) {
-    //     return console.log(err);
-    //   }
+    var jsonData = JSON.stringify(kailas);
+
+
+    if (fs.existsSync('./' + event.sender_id + '.json')) {
+      fs.appendFile('./' + event.sender_id + '.json', jsonData, function (err) {
+        if (err) throw err;
+      });
+    }
+    else {
+      fs.writeFile('./' + event.thread_id + '.json', jsonData, 'utf8');
+    }
+
+    api.sendMessage("Saved! We'll be asking this soon!", event.thread_id);
+
+
+};
+
+
+
+
+    // fs.readFile('./' + event.sender_id, handleFile)
+    //
+    // function handleFile(err, data) {
+    //     if (err) throw err
+    //     obj = JSON.parse(data)
+    // }
+
+
+
+    // var stream = fs.createWriteStream("storage.json");
+    // stream.once('open', function(fd) {
+    //   stream.write(jsonData);
+    //   stream.end();
     // });
-    var stream = fs.createWriteStream("storage.json");
-    stream.once('open', function(fd) {
-      stream.write(jsonData);
-      stream.end();
-    });
-    api.sendMessage("Remembered! I'll be asking you this soon, so don't forget!", event.thread_id);
-
-  };
+    // api.sendMessage("Remembered! I'll be asking you this soon, so don't forget!", event.thread_id);
